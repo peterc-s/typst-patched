ed7d1943b99f43b93e4ec2ee84b1ae0d8133a5e2
peterc-s <petercs@purelymail.com>
2026-02-25 18:09:03 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Typst - patched with newer hayagriva";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+
+        patchedTypst = pkgs.typst.overrideAttrs (old: {
+          patches = [./hayagriva.patch];
+          cargoDeps = old.cargoDeps.overrideAttrs (old: {
+            vendorStaging = old.vendorStaging.overrideAttrs {
+              patches = [./hayagriva.patch];
+              outputHash = "sha256-5yOkNmnNJt/gLzU7Pwn751vzOxj0f6lriKj+NnUa1t4=";
+            };
+          });
+        });
+      in {
+        packages.patched-typst = patchedTypst;
+        defaultPackage = patchedTypst;
+      }
+    );
+}
